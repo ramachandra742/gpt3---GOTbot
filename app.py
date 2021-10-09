8c223692ac33d7fc9d2023abac0ecfc2cf009229
Ramachandra.N <ramachandra742@gmail.com>
2021-10-09 12:12:39 +0530

    updated app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from gotbot import ask, append_interaction_to_chat_log
 app = Flask(__name__)
 
 # if for some reasons the conversion got weird, change the secret key
-app.config['SECRET_KEY'] = '8kj6yd865dgh'
+app.config['SECRET_KEY'] = '8kj6yd865lop8'
 
 @app.route('/gotbot', methods=['POST'])
 def got():
